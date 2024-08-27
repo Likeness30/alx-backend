a73742fe6e1b36c6ad84ccf151730fe5b048d71c
Rufus <likeness14@gmail.com>
2024-08-27 19:08:47 +0100

    0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-
-"""A simple flask app"""
-
+"""
+Flask app
+"""
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route("/", methods=["GET"])
-def home():
-    """Get the locale from request"""
-    return render_template("0-index.html")
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(port="5000", host="0.0.0.0", debug=True)
